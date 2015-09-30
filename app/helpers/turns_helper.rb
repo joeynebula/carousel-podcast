1022cd2735e341d94b6436265633207110cbd9b2
joeynebula <joey.baker@waterfield.com>
2015-09-30 18:25:59 -0500

    helper for audio player part 2

--- app/helpers/turns_helper.rb
+++ app/helpers/turns_helper.rb
@@ -5,4 +5,8 @@ module TurnsHelper
     margin-right: auto\"></img>"
     return html
   end
+  def output_audio_player(turn_id)
+    #3830885
+    return "<iframe style=\"border: none\" src=\"//html5-player.libsyn.com/embed/episode/id/#{turn_id}/height/325/width/325/theme/legacy/direction/no/autoplay/no/autonext/no/thumbnail/yes/preload/no/no_addthis/no/\" height=\"325\" width=\"325\" scrolling=\"no\"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>"
+  end
 end
