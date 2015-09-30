6764249eb05f0330bedbf94fcd10497d5f12e5a2
joeynebula <joey.baker@waterfield.com>
2015-09-29 19:55:23 -0500

    fixing the carousel huh huh

--- app/helpers/turns_helper.rb
+++ app/helpers/turns_helper.rb
@@ -1,2 +1,8 @@
 module TurnsHelper
+  def output_image(url)
+    html = "<img src=\"#{url}\" style=\"display: block;
+    margin-left: auto;
+    margin-right: auto\"></img>"
+    return html
+  end
 end
