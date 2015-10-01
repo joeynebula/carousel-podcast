58d07df23322adf204b2fa4c9165ec9b91b19d2f
joeynebula <joey.baker@waterfield.com>
2015-10-01 16:44:48 -0500

    adding the audio player to the turns helpers

--- app/helpers/turns_helper.rb
+++ app/helpers/turns_helper.rb
@@ -7,6 +7,11 @@ module TurnsHelper
   end
   def output_audio_player(turn_id)
     #3830885
-    return "<iframe style=\"border: none\" src=\"//html5-player.libsyn.com/embed/episode/id/#{turn_id}/height/325/width/325/theme/legacy/direction/no/autoplay/no/autonext/no/thumbnail/yes/preload/no/no_addthis/no/\" height=\"325\" width=\"325\" scrolling=\"no\"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>"
+    return "<iframe style=\"border: none\" src=\"
+    //html5-player.libsyn.com/embed/episode/id/#{turn_id}
+    /height/325/width/325/theme/legacy/direction/no/autoplay/no/
+    autonext/no/thumbnail/yes/preload/no/no_addthis/no/\" height=\"325\"
+     width=\"325\" scrolling=\"no\"  allowfullscreen webkitallowfullscreen
+      mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>"
   end
 end
