490545c145f5685d652e6b7843ed51c1cd3918e4
joeynebula <joey.baker@waterfield.com>
2015-10-01 21:02:06 -0500

    removing audio_url validation

--- app/models/turn.rb
+++ app/models/turn.rb
@@ -2,8 +2,6 @@ class Turn < ActiveRecord::Base
 attr_accessor :title
   validates :title, presence: true,
             length: { minimum: 5 }
-  validates :audio_link, presence: true,
-            length: { minimum: 10 }
   validates :cover_url, presence: true,
             length: { minimum: 10 }
   validates :cast_date, presence: true
